a9a3e02dddbd622261db298b538d8714321b8784
shodayuki <yshoda1227@gmail.com>
2020-08-11 23:01:47 +0900

    環境構築

--- 05_JavaScriptを実践で使う/040_演習_スクロール監視×スライドアニメーション/end/main.js
+++ 05_JavaScriptを実践で使う/040_演習_スクロール監視×スライドアニメーション/end/main.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const cb = function (el, isIntersecting) {
+    if(isIntersecting) {
+      el.classList.add('inview');
+    }
+  }
 
-    const cb = function (el, isIntersecting) {
-        if(isIntersecting) {
-            el.classList.add('inview');
-        }
-    } 　
-
-    const so = new ScrollObserver('.cover-slide', cb);
+  const so = new ScrollObserver('.cover-slide', cb);
 });
-
